0ba68362f961f6dfba7e3f617c5c0f21c1b77cdf
Cao Xuerui <caoxuerui2011@foxmail.com>
2019-06-02 11:59:27 +0800

    1.提交遗失文件

---
+++ QICIFutures/Find/TheMarktAllFuturesViewController/TheMarktAllForeginView.h
@@ -0,0 +1,38 @@
+//
+//  TheMarktAllForeginView.h
+//  QICIFutures
+//
+//  Created by mac on 2019/6/1.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AllFuturesTopBtnView.h"
+
+typedef enum : NSUInteger {
+    AllFuturesInfoTypeForeignGoods,
+    AllFuturesInfoTypeForeignGuzhi,
+    AllFuturesInfoTypeForeignWaihui,
+} AllFuturesInfoTypeForeign;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TheMarktAllForeginView : UIView<UITableViewDelegate,UITableViewDataSource>
+
+@property (nonatomic,strong) UITableView *listView;
+
+-(void)refreshWithType:(AllFuturesInfoTypeForeign)type;
+
+@property (nonatomic,strong) NSMutableArray *dataArray;
+
+@property (nonatomic,strong) NSMutableDictionary *dataDic;
+
+@property (nonatomic,assign) AllFuturesInfoTypeForeign type;
+
+@property (nonatomic,strong) AllFuturesTopBtnView *btnView;
+
+@property (nonatomic,copy) void(^selectRowBlock)(NSDictionary *dic);
+
+@end
+
+NS_ASSUME_NONNULL_END
