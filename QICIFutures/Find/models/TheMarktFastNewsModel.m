592265ea36ec01b06c2019d03324dc3f1e93ffeb
Cao Xuerui <caoxuerui2011@foxmail.com>
2019-06-03 17:01:50 +0800

    1.行情页修复滚动快讯显示HTML标签的问题

--- QICIFutures/Find/models/TheMarktFastNewsModel.m
+++ QICIFutures/Find/models/TheMarktFastNewsModel.m
@@ -16,6 +16,28 @@
         NSString *time = [TheMarktTools getDateStringWithTimeStr:value];
         [super setValue:time forKey:key];
         
+    }else if([key isEqualToString:@"Title"]){
+        
+        NSScanner *theScanner;
+        
+        NSString *text = nil;
+        NSString *str = (NSString *)value;
+        
+        theScanner = [NSScanner scannerWithString:str];
+        
+        while ([theScanner isAtEnd] == NO) {
+            
+            // find start of tag
+            [theScanner scanUpToString:@"<" intoString:NULL] ;
+            
+            // find end of tag
+            [theScanner scanUpToString:@">" intoString:&text] ;
+            
+            // replace the found tag with a space
+            //(you can filter multi-spaces out later if you wish)
+            str = [str stringByReplacingOccurrencesOfString:[NSString stringWithFormat:@"%@>", text] withString:@""];
+        }
+        [super setValue:str forKey:key];
     }else{
         [super setValue:value forKey:key];
     }
